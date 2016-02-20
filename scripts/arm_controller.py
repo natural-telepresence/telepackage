7984fdb7694632d2911fd9da61efab349facdbb7
Eric Yeh <ericyeh1995@gmail.com>
2016-02-20 03:01:24 -0600

    update controller

--- scripts/arm_controller.py
+++ scripts/arm_controller.py
@@ -8,9 +8,9 @@ def motor(data):
 
 def controller():
     rospy.init_node('controller')
-    pub1 = rospy.Publisher("/motor_1/command", Float64, queue size=10)
-    pub2 = rospy.Publisher("/motor_2/command", Float64, queue size=10)
-    pub3 = rospy.Publisher("/motor_3/command", Float64, queue size=10)
+    pub1 = rospy.Publisher("/motor_x/command", Float64, queue size=10)
+    pub2 = rospy.Publisher("/motor_y/command", Float64, queue size=10)
+    pub3 = rospy.Publisher("/motor_z/command", Float64, queue size=10)
     sub = rospy.Subscriber("/cardboard/imu", Imu, motor) 
 
     rospy.spin()
