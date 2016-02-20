844612d2542e39da1583213e4e21be35882f6414
Eric Yeh <ericyeh1995@gmail.com>
2016-02-20 03:09:43 -0600

    raphael did something

--- scripts/arm_controller.py
+++ scripts/arm_controller.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float64
-from sensor_msgs.msg import Imu 
+from sensor_msgs.msg import Imu
 
-def motor(data):
-    pass
+class GimbalController(object):
+	def __init__(self):
+		self.pubx = rospy.Publisher("/motor_x/command", Float64, queue_size = 10)
+		self.puby = rospy.Publisher("/motor_y/command", Float64, queue_size = 10)
+		self.pubz = rospy.Publisher("/motor_z/command", Float64, queue_size = 10)
+		self.sub = rospy.Subscriber("/cardboard/imu", Imu, self.imu) 
 
-def controller():
-    rospy.init_node('controller')
-    pub1 = rospy.Publisher("/motor_x/command", Float64, queue size=10)
-    pub2 = rospy.Publisher("/motor_y/command", Float64, queue size=10)
-    pub3 = rospy.Publisher("/motor_z/command", Float64, queue size=10)
-    sub = rospy.Subscriber("/cardboard/imu", Imu, motor) 
+	def imu(self, data):
+		euler = tf.transformations.euler_from_quaternion(data.orientation)
+		roll = euler[0]
+		pitch = euler[1]
+		yaw = euler[2]
+		self.pubx.publish(roll + 2.62)
+		self.puby.publish(pitch + 2.62)
+		self.pubz.publish(yaw + 2.546)
 
-    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        controller()
-    except rospy.ROSInterruptException:
+	try:
+		rospy.init_node('controller')
+		c = GimbalController()
+		rospy.spin()
+	except rospy.ROSInterruptException:
 		pass
