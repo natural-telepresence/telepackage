508680e7c70c6c7bc16f98b535a90f50a634bcd4
Brent Yi <yibrenth@gmail.com>
2016-02-20 19:39:15 -0600

    motor control script progress, pin #s need fixing

--- scripts/motor_control.py
+++ scripts/motor_control.py
@@ -8,13 +8,15 @@ import time
 class motor_control(object):
     def __init__(self):
         #sub joystick x,y
-        self.joy = rospy.Subscriber("/controller/input", Twist, self.callback)
+        self.joy = rospy.Subscriber("/base/cmd_vel", Twist, self.callback)
 
-        left_motor_pin = mraa.Pwm(3)
+        left_pin_num = rospy.get_param('left_motor_pin', 4) 
+        left_motor_pin = mraa.Pwm(left_pin_num)
         left_motor_pin.period_us(20000)
         left_motor_pin.enable(True)
 
-        right_motor_pin = mraa.Pwm(4)
+        right_pin_num = rospy.get_param('right_motor_pin', 3)
+        right_motor_pin = mraa.Pwm(right_pin_num)
         right_motor_pin.period_us(20000)
         right_motor_pin.enable(True)
 
@@ -24,10 +26,8 @@ class motor_control(object):
         left_motor = fwd_speed + turn_speed
         right_motor = fwd_speed - turn_speed
 
-        left_motor_pin.write(((0.5*(left_motor+1))+1)*0.0005)
-        right_motor_pin.write(((0.5*(right_motor+1))+1)*0.0005)
-
-
+        left_motor_pin.config_ms(20, 1.5 + 0.5 * left_motor);
+        right_motor_pin.config_ms(20, 1.5 + 0.5 * right_motor);
 
 if __name__ == '__main__':
     rospy.init_node('robot')
